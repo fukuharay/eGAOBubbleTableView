8cfe1e1f4b41ecce84091ec4e14af40f83370bb7
fukuharay <fukuharay@egao-inc.co.jp>
2014-09-23 01:31:02 +0900

    Changes to be committed: 	new file:   .DS_Store 	modified:   README.md 	new file:   example/.DS_Store 	new file:   example/UIBubbleTableViewExample.xcodeproj/project.pbxproj 	new file:   example/UIBubbleTableViewExample.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   example/UIBubbleTableViewExample.xcodeproj/project.xcworkspace/xcshareddata/UIBubbleTableViewExample.xccheckout 	new file:   example/UIBubbleTableViewExample.xcodeproj/project.xcworkspace/xcuserdata/himawari.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   example/UIBubbleTableViewExample.xcodeproj/xcuserdata/himawari.xcuserdatad/xcschemes/UIBubbleTableViewExample.xcscheme 	new file:   example/UIBubbleTableViewExample.xcodeproj/xcuserdata/himawari.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   example/UIBubbleTableViewExample/.DS_Store 	new file:   example/UIBubbleTableViewExample/AppDelegate.h 	new file:   example/UIBubbleTableViewExample/AppDelegate.m 	new file:   example/UIBubbleTableViewExample/UIBubbleTableViewExample-Info.plist 	new file:   example/UIBubbleTableViewExample/UIBubbleTableViewExample-Prefix.pch 	new file:   example/UIBubbleTableViewExample/ViewController.h 	new file:   example/UIBubbleTableViewExample/ViewController.m 	new file:   example/UIBubbleTableViewExample/en.lproj/.DS_Store 	new file:   example/UIBubbleTableViewExample/en.lproj/InfoPlist.strings 	new file:   example/UIBubbleTableViewExample/en.lproj/ViewController.xib 	new file:   example/UIBubbleTableViewExample/main.m 	new file:   example/avatar1.png 	new file:   example/icon_114.png 	new file:   example/icon_57.png 	new file:   images/bubbleMine.png 	new file:   images/bubbleMine@2x.png 	new file:   images/bubbleSomeone.png 	new file:   images/bubbleSomeone@2x.png 	new file:   images/missingAvatar.png 	new file:   images/missingAvatar@2x.png 	new file:   images/typingMine.png 	new file:   images/typingMine@2x.png 	new file:   images/typingSomeone.png 	new file:   images/typingSomeone@2x.png 	new file:   psd/myBubble@2x.psd 	new file:   psd/myTyping@2x.psd 	new file:   psd/someoneBubble@2x.psd 	new file:   psd/someoneTyping@2x.psd 	new file:   src/.DS_Store 	new file:   src/NSBubbleData.h 	new file:   src/NSBubbleData.m 	new file:   src/UIBubbleHeaderTableViewCell.h 	new file:   src/UIBubbleHeaderTableViewCell.m 	new file:   src/UIBubbleTableView.h 	new file:   src/UIBubbleTableView.m 	new file:   src/UIBubbleTableViewCell.h 	new file:   src/UIBubbleTableViewCell.m 	new file:   src/UIBubbleTableViewDataSource.h 	new file:   src/UIBubbleTypingTableViewCell.h 	new file:   src/UIBubbleTypingTableViewCell.m

---
+++ example/UIBubbleTableViewExample/ViewController.m
@@ -0,0 +1,149 @@
+//
+//  ViewController.m
+//
+//  Created by Alex Barinov
+//  Project home page: http://alexbarinov.github.com/UIBubbleTableView/
+//
+//  This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
+//  To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/
+//
+
+// 
+// Images used in this example by Petr Kratochvil released into public domain
+// http://www.publicdomainpictures.net/view-image.php?image=9806
+// http://www.publicdomainpictures.net/view-image.php?image=1358
+//
+
+#import "ViewController.h"
+#import "UIBubbleTableView.h"
+#import "UIBubbleTableViewDataSource.h"
+#import "NSBubbleData.h"
+
+@interface ViewController ()
+{
+    IBOutlet UIBubbleTableView *bubbleTable;
+    IBOutlet UIView *textInputView;
+    IBOutlet UITextField *textField;
+
+    NSMutableArray *bubbleData;
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSBubbleData *heyBubble = [NSBubbleData dataWithText:@"ハートのメロンパン" date:[NSDate dateWithTimeIntervalSinceNow:-300] type:BubbleTypeMine];
+    heyBubble.avatar = [UIImage imageNamed:@"avatar2.jpg"];
+
+    NSBubbleData *photoBubble = [NSBubbleData dataWithImage:[UIImage imageNamed:@"c48.jpg"] date:[NSDate dateWithTimeIntervalSinceNow:-290] type:BubbleTypeSomeoneElse];
+    photoBubble.avatar = [UIImage imageNamed:@"avatar1.png"];
+    
+    NSBubbleData *replyBubble = [NSBubbleData dataWithText:@"チーズケーキ" date:[NSDate dateWithTimeIntervalSinceNow:-100] type:BubbleTypeMine];
+    replyBubble.avatar = [UIImage imageNamed:@"avatar2.jpg"];
+    
+    NSBubbleData *photoBubble2 = [NSBubbleData dataWithImage:[UIImage imageNamed:@"s139.jpg"] date:[NSDate dateWithTimeIntervalSinceNow:-5] type:BubbleTypeSomeoneElse];
+    photoBubble2.avatar = [UIImage imageNamed:@"avatar1.png"];
+   
+    bubbleData = [[NSMutableArray alloc] initWithObjects:heyBubble, photoBubble, photoBubble2, replyBubble,nil];
+    bubbleTable.bubbleDataSource = self;
+    
+    // The line below sets the snap interval in seconds. This defines how the bubbles will be grouped in time.
+    // Interval of 120 means that if the next messages comes in 2 minutes since the last message, it will be added into the same group.
+    // Groups are delimited with header which contains date and time for the first message in the group.
+    
+    bubbleTable.snapInterval = 120;
+    
+    // The line below enables avatar support. Avatar can be specified for each bubble with .avatar property of NSBubbleData.
+    // Avatars are enabled for the whole table at once. If particular NSBubbleData misses the avatar, a default placeholder will be set (missingAvatar.png)
+    
+    bubbleTable.showAvatars = YES;
+    
+    // Uncomment the line below to add "Now typing" bubble
+    // Possible values are
+    //    - NSBubbleTypingTypeSomebody - shows "now typing" bubble on the left
+    //    - NSBubbleTypingTypeMe - shows "now typing" bubble on the right
+    //    - NSBubbleTypingTypeNone - no "now typing" bubble
+    
+    bubbleTable.typingBubble = NSBubbleTypingTypeSomebody;
+    
+    [bubbleTable reloadData];
+    
+    // Keyboard events
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+#pragma mark - UIBubbleTableViewDataSource implementation
+
+- (NSInteger)rowsForBubbleTable:(UIBubbleTableView *)tableView
+{
+    return [bubbleData count];
+}
+
+- (NSBubbleData *)bubbleTableView:(UIBubbleTableView *)tableView dataForRow:(NSInteger)row
+{
+    return [bubbleData objectAtIndex:row];
+}
+
+#pragma mark - Keyboard events
+
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+
+    [UIView animateWithDuration:0.2f animations:^{
+        
+        CGRect frame = textInputView.frame;
+        frame.origin.y -= kbSize.height;
+        textInputView.frame = frame;
+        
+        frame = bubbleTable.frame;
+        frame.size.height -= kbSize.height;
+        bubbleTable.frame = frame;
+    }];
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    [UIView animateWithDuration:0.2f animations:^{
+        
+        CGRect frame = textInputView.frame;
+        frame.origin.y += kbSize.height;
+        textInputView.frame = frame;
+        
+        frame = bubbleTable.frame;
+        frame.size.height += kbSize.height;
+        bubbleTable.frame = frame;
+    }];
+}
+
+#pragma mark - Actions
+
+- (IBAction)sayPressed:(id)sender
+{
+    bubbleTable.typingBubble = NSBubbleTypingTypeNobody;
+
+    NSBubbleData *sayBubble = [NSBubbleData dataWithText:textField.text date:[NSDate dateWithTimeIntervalSinceNow:0] type:BubbleTypeMine];
+    sayBubble.avatar = [UIImage imageNamed:@"avatar2.jpg"];
+    [bubbleData addObject:sayBubble];
+    [bubbleTable reloadData];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+}
+
+@end
